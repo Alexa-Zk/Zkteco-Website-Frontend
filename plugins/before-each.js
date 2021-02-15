14c89963e0609280f8891addda3466cb056e0e93
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2021-02-15 09:00:48 +0100

    Remove preloader

--- plugins/before-each.js
+++ plugins/before-each.js
@@ -1,11 +1,11 @@
 export default async ({ app, store }) => {
-    app.router.beforeEach((to, from, next) => {
-        if (to.path.includes("/website") || to.path === "/") {
-            store.commit('app/setPreloader', true);
-            next();
-        } else {
-            store.commit('app/setPreloaderStore', true)
-            next();
-        }
-    });
+    // app.router.beforeEach((to, from, next) => {
+    //     if (to.path.includes("/website") || to.path === "/") {
+    //         store.commit('app/setPreloader', true);
+    //         next();
+    //     } else {
+    //         store.commit('app/setPreloaderStore', true)
+    //         next();
+    //     }
+    // });
 };
--- plugins/routes.js
+++ plugins/routes.js
@@ -1,14 +1,14 @@
 export default async ({ app, store }) => {
-    app.router.afterEach((to, from) => {
-      if (to.path.includes("/website") || to.path === "/") {
-        setTimeout(() => {
-          store.commit('app/setPreloader', false)
-        }, 3000)
-      } else {
-        setTimeout(() => {
-          store.commit('app/setPreloaderStore', false)
-        }, 3000)
-      }      
-    });
+    // app.router.afterEach((to, from) => {
+    //   if (to.path.includes("/website") || to.path === "/") {
+    //     setTimeout(() => {
+    //       store.commit('app/setPreloader', false)
+    //     }, 3000)
+    //   } else {
+    //     setTimeout(() => {
+    //       store.commit('app/setPreloaderStore', false)
+    //     }, 3000)
+    //   }      
+    // });
   
   }
