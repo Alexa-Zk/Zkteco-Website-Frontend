63f18ab983127dfe1996eee7c07713b93361b9a3
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2021-03-03 11:16:56 +0100

    include components

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,54 @@
 <template lang="html">
     <main id="homepage-1">
-        <h1>This is the Home page</h1>
-        <!-- <home-banner :homeSlider="adSliders" />
+        <home-banner :homeSlider="adSliders" />
         <site-feautures-fullwidth />
         <div class="ps-container">
             <related-posts />
         </div>
         <home-brand :partnersLogo="ourPartners" />
-        <newsletters layout="fullwidth" /> -->
+        <newsletters layout="fullwidth" />
     </main>
 </template>
 <script>
-// import { mapState } from "vuex";
-// import HomeBanner from '~/components/partials/homepage/website/default/HomeBanner';
-// import Newsletters from '~/components/partials/commons/website/Newsletters';
-// import SiteFeauturesFullwidth from '~/components/partials/commons/website/SiteFeaturesFullwidth';
-// import RelatedPosts from '~/components/partials/post/website/RelatedPosts';
-// import HomeBrand from '~/components/partials/shop/sections/website/ShopBrands';
+import { mapState } from "vuex";
+import HomeBanner from '~/components/partials/homepage/website/default/HomeBanner';
+import Newsletters from '~/components/partials/commons/website/Newsletters';
+import SiteFeauturesFullwidth from '~/components/partials/commons/website/SiteFeaturesFullwidth';
+import RelatedPosts from '~/components/partials/post/website/RelatedPosts';
+import HomeBrand from '~/components/partials/shop/sections/website/ShopBrands';
 
 export default {
     data() {
         return {
         };
     },
-    // components: {
-    //     HomeBanner,
-    //     HomeBrand,
-    //     SiteFeauturesFullwidth,
-    //     Newsletters,
-    //     RelatedPosts
-    // },
+    components: {
+        HomeBanner,
+        HomeBrand,
+        SiteFeauturesFullwidth,
+        Newsletters,
+        RelatedPosts
+    },
 
-    // transition: 'zoom',
+    transition: 'zoom',
     layout: 'empty',
 
-    // computed: {
-    //     ...mapState({
-    //         homePage: state => state.website.homePage
-    //     }),
-    //     adSliders() {
-    //         return this.homePage ? this.homePage[0].sliders : [];
-    //     },
-    //     ourPartners() {
-    //         return this.homePage ? this.homePage[0].partners.slice(0, 8) : [];
-    //     }
+    computed: {
+        ...mapState({
+            homePage: state => state.website.homePage
+        }),
+        adSliders() {
+            return this.homePage ? this.homePage[0].sliders : [];
+        },
+        ourPartners() {
+            return this.homePage ? this.homePage[0].partners.slice(0, 8) : [];
+        }
         
-    // },
-    // created() {
-    //     let payload = {};
-    //     const response = this.$store.dispatch('website/getArticles', payload);
-    //     const homepage = this.$store.dispatch('website/getHomepage', payload);
-    // }
+    },
+    created() {
+        let payload = {};
+        const response = this.$store.dispatch('website/getArticles', payload);
+        const homepage = this.$store.dispatch('website/getHomepage', payload);
+    }
 };
 </script>
