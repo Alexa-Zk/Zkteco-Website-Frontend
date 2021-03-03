729715528bed9cb2a5f3a8e5f3c16f43bc0d3235
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2021-03-03 10:32:36 +0100

    Remove template and add to empty layout

---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <client-only>
+        <v-app>
+            <v-main>
+                
+                <nuxt></nuxt>
+            </v-main>
+        </v-app>
+    </client-only>
+</template>
+
+<script>
+
+export default {
+};
+</script>
+
+<style lang="scss" scoped></style>
