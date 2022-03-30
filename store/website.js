c68cd8e1eebd31e643dc5b95506c0d03e0bc717b
Chinedu Ohagwu <chinedu@youverify.co>
2022-03-30 23:34:10 +0100

    Request a quote

--- store/website.js
+++ store/website.js
@@ -139,6 +139,18 @@ export const actions = {
         return reponse;
     },
 
+    async requestAQuote({ commit }, payload) {
+        commit('setLoading', true);
+
+        const reponse = await Repository.post(`${subBaseUrl}/request-a-quotes`, payload)
+            .then(response => {
+                commit('setLoading', false);
+                return response.data;
+            })
+            .catch(error => ({ error: JSON.stringify(error) }));
+        return reponse;
+    },
+
     async getArticles({ commit, state }, payload) {
         commit('setLoading', true);
         let params = {
