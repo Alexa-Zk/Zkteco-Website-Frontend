b070b84c19dd58ecfee97d2aad4b0782f498c1cd
tobilobabadrun@gmail.com <apple@Apples-MacBook-Pro.local>
2022-06-06 11:42:07 +0100

    worked on product image centralisation

--- components/elements/product/website/ProductCategoryDefault.vue
+++ components/elements/product/website/ProductCategoryDefault.vue
@@ -114,13 +114,12 @@ export default {
 <style lang="scss" scoped>
 .ps-product {
     .ps-product__thumbnail {
-        max-width: 100%;
-        max-height: 100%;
+        width: 208px;
+        height: 208px;
         @media (max-width: 480px) {
             width: 100%;
         }
         img {
-            height: 100%;
             object-fit: contain;
         }
     }
