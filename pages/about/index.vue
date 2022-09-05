18675d9d077efa42821535b30c91c62d4168536b
Adesanwo Kazeem <adesanwo1@yahoo.com>
2022-09-05 14:35:29 +0100

    working on about us

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,11 +1,292 @@
 <template lang="html">
     <div class="ps-page--single">
-        <img data-src="/img/website/about-us-banner.jpg" class="lazyload" alt="" />
-        <bread-crumb :breadcrumb="breadCrumb" />
+        <img
+            data-src="/img/website/about-us-banner.jpg"
+            class="lazyload"
+            alt=""
+        />
+        <!--bread-crumb :breadcrumb="breadCrumb" / -->
         <!-- <our-team /> -->
-        <about-value />
+        <!--about-value />
         <about-mission />
-        <about-vision />
+        <about-vision / -->
+
+        <div class="group_profile_container">
+            <h3>About US</h3>
+            <div class="group_profile">
+                <div>
+                    <h4>Group Profile</h4>
+                    <div>
+                        <p>
+                            ZKTECO CO., LTD. (ZKTeco) (301330: CN Shenzhen Stock
+                            Exchange) is an internationally renowned enterprise
+                            which specializes in pioneering biometric
+                            recognition techniques, and provides products and
+                            solutions of smart entrance management, smart
+                            identity authentication and smart office. ZKTeco
+                            offers services on all fronts to public-service,
+                            enterprise-level and personal users.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/group_profile/x-add3.png"
+                        class="lazyload"
+                        alt=""
+                        width="40"
+                        height="30"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="emp_stat_container">
+            <div class="emp_stat">
+                <div>
+                    <span>
+                        3800+
+                    </span>
+                    <p>Global Employees Exceed</p>
+                </div>
+                <div>
+                    <span>
+                        1100+
+                    </span>
+                    <p>R&D Staffs Exceed</p>
+                </div>
+                <div>
+                    <span>730 </span>
+                    <p>Number of Foreign Employees</p>
+                </div>
+                <div>
+                    <span>38</span>
+                    <p>Overseas Subsidiaries</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon_container">
+            <div class="icon_wrapper">
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/education.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Education </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Construction.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Construction </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Energy.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Energy </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Supply_Chain.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Supply Chain</span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Finance.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Finance </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Health_Care.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Health Care </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Hotel_Management.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Hotel Management </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Public_Facilities.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Public Facilities </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Residential.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Residential </span>
+                </div>
+                <div>
+                    <img
+                        data-src="/img/about_us/Application_areas_icon/Retail.png"
+                        class="lazyload"
+                        alt=""
+                    />
+                    <span> Retail </span>
+                </div>
+            </div>
+        </div>
+        <div class="main_business">
+            <div class="main_business_wrapper">
+                <h3>Main Business</h3>
+                <div class="business_content">
+                    <div class="business_emp">
+                        <div>
+                            <img
+                                data-src="/img/about_us/Empowerment_Integrated/empowerment.jpg"
+                                class="lazyload"
+                                alt=""
+                            />
+                        </div>
+                        <div>
+                            <h4>
+                                Empowerment Integrated Business based on
+                                Biometric Verication Core Techniques (Smart
+                                Identity Authentication Application)
+                            </h4>
+                            <div>
+                                <p>
+                                    ZKTeco is a globally renowned provider of
+                                    biometric verication algorithm techniques,
+                                    sensors and software platform, which owns
+                                    patents of ngerprint, iris, face, vein and
+                                    palm print recognition techniques etc., and
+                                    owns patents of compute vision techniques
+                                    including facial recognition, identication
+                                    of behaviors, X-ray object identication, and
+                                    video structure, which make ZKTeco one of
+                                    the enterprises in the industry that owns
+                                    comparatively numerous patents. From the
+                                    core technical authorizations of physical
+                                    and behavioral features including ngerprint,
+                                    iris, face, and vein, to ZKBioDeSee Software
+                                    empowered by hybrid biometric verication and
+                                    computer vision and hybrid biometric
+                                    verication identity authentication O2O smart
+                                    terminals and solutions, we offer one-stop
+                                    solutions based on smart verication and
+                                    identity authentication applications for
+                                    vertical industries, partners, and system
+                                    integrators.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="business_entrance">
+                        <div>
+                            <h4>
+                                Smart Entrance & Exit “Person + Car +
+                                Object”Software Platform Empowerment Integrated
+                                Business (Smart Entrance & Exit Application)
+                            </h4>
+                            <div>
+                                <p>
+                                    ZKTeco is a globally renowned provider of
+                                    core techniques, smart terminals and
+                                    software platforms based on smart entrance &
+                                    exit “car + person + object” applications,
+                                    and owns a large quantity of software
+                                    copyrights of software products and
+                                    solutions and patented smart terminal
+                                    patented techniques, and strives to
+                                    integrate hybrid biometric verication
+                                    techniques and computer vision techniques
+                                    with the Internet, IoT, big data and cloud
+                                    computing, and builds system platform
+                                    including ZKBioSecurity and ZKBioCV Secruity
+                                    etc. ZKTeco owns professional development
+                                    and supply chain teams for smart terminal of
+                                    person, car and object passages, and
+                                    globally famous entrance & exit biometric
+                                    verication computer vision smart IoT
+                                    hardware and servers, private cloud and
+                                    public linkage services to provide system
+                                    integration services for many partners
+                                    regarding industry solutions, which are
+                                    widely applied in various elds including
+                                    smart traffic, smart nance, smart education,
+                                    smart medicine and smart government etc.
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <img
+                                data-src="/img/about_us/Smart_Entrance/smart_entrance.jpg"
+                                class="lazyload"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div class="business_emp">
+                        <div>
+                            <img
+                                data-src="/img/about_us/Smart_Office/smart_office.jpg"
+                                class="lazyload"
+                                alt=""
+                            />
+                        </div>
+                        <div>
+                            <h4>
+                                ZKTeco + Smart Office Cloud Platform Empowerment
+                                Integrated Business (Smart Office Application)
+                            </h4>
+                            <div>
+                                <p>
+                                    ZKTeco is a globally renowned provider of
+                                    ZKTeco+ Cloud Platform empowered core
+                                    techniques, smart terminals and software
+                                    platform, which strikes ZKTeco+ Cloud
+                                    Platform is built for the establishment of
+                                    the “Time Management + Space Management”
+                                    duo-core concept, and the private cloud
+                                    platform with high security level of date
+                                    archive. The service desk, offerings
+                                    information service, app. store and
+                                    authentication authorization service, API
+                                    service platform and cloud storage service
+                                    etc. <br />
+                                    <br />
+                                    <br />
+                                    “Cloud Application” connected through
+                                    ZKTeco+ links up smart terminal devices in
+                                    smart office applications to IoT devices of
+                                    attendance, access control, visitor
+                                    management and POS, and smart terminal
+                                    devices are connected through software
+                                    platforms including ZKBioTime and ZKBio
+                                    Cloud etc., which offers one-stop O2O
+                                    solutions for enterprise smart office that
+                                    are widely applied in various elds including
+                                    smart office and smart education etc.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -54,6 +335,152 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.group_profile_container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 8%;
+    margin-right: 8%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    flex-direction: column;
+    .group_profile {
+        display: grid;
+        grid-template-columns: 3fr 1fr;
+        grid-gap: 2em;
+
+        h4 {
+            color: #8bc76f;
+        }
+    }
+    @media (max-width: 540px) {
+        .group_profile {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+}
+
+.emp_stat_container {
+    background: #8bc63e;
+    .emp_stat {
+        margin-left: 8%;
+        margin-right: 8%;
+        padding-top: 50px;
+        padding-bottom: 50px;
+
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+
+        div {
+            color: #fff;
+            span {
+                font-size: 3.8rem;
+                font-weight: bolder;
+            }
+            p {
+                color: #fff;
+                font-size: 1.5rem;
+            }
+        }
+    }
+    @media (max-width: 540px) {
+        .emp_stat {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            text-align: center;
+        }
+    }
+}
+
+.icon_container {
+    .icon_wrapper {
+        margin-left: 18%;
+        margin-right: 18%;
+        padding-top: 50px;
+        padding-bottom: 50px;
+
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-gap: 2rem;
+
+        div {
+            padding-top: 40px;
+            padding-bottom: 18px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            justify-items: center;
+
+            img {
+                width: 9rem;
+                height: 9rem;
+            }
+            span {
+                padding-top: 5px;
+            }
+        }
+    }
+    @media (max-width: 540px) {
+        .icon_wrapper {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            text-align: center;
+        }
+    }
+}
+.main_business {
+    .main_business_wrapper {
+        margin-left: 8%;
+        margin-right: 8%;
+        padding-top: 30px;
+        padding-bottom: 50px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        h3 {
+            padding-bottom: 10px;
+        }
+
+        .business_content {
+            h4 {
+                color: #8bc76f;
+            }
+            .business_emp {
+                padding-top: 20px;
+                padding-bottom: 60px;
+                display: grid;
+                grid-template-columns: 1fr 3fr;
+                grid-gap: 2em;
+            }
+
+            .business_entrance {
+                padding-top: 40px;
+                padding-bottom: 60px;
+                display: grid;
+                grid-template-columns: 3fr 1fr;
+                grid-gap: 2em;
+            }
+        }
+
+        @media (max-width: 540px) {
+            .business_content {
+                .business_emp {
+                    display: flex;
+                    flex-direction: column;
+                }
+                .business_entrance {
+                    display: flex;
+                    flex-direction: column;
+                }
+            }
+        }
+    }
+}
+
 .ps-page--single {
     img {
         height: 200px;
