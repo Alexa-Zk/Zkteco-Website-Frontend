7346e25f44528b472853049612e888c14a736301
Adesanwo Kazeem <adesanwo1@yahoo.com>
2023-02-06 16:35:35 +0100

    worked on software download

---
+++ pages/softwar/test.vue
@@ -0,0 +1,435 @@
+<template lang="html">
+    <div class="ps-page--single ps-page--vendor">
+        <bread-crumb :breadcrumb="breadCrumb" />
+        <!--div class="ps-vendor-banner bg--cover"></div -->
+
+        <div class="softwarebanner">
+            <p>
+                Making Access control and Time Management easier with our
+                industry-leading software and innovation. <br />
+                <button>Read More</button>
+            </p>
+        </div>
+
+        <div class="softwarecontainer">
+            <div class="wrapper_001">
+                <div class="container">
+                    <div class="content">
+                        <div class="header">
+                            <p>
+                            <h4> Update Alert </h4>
+                                Version V5000 4.1.1 Replaces V3.1.1 (Size : 1.19 GB)
+                            </p>
+                        </div>
+                        <div class="wrapper">
+                            <p>
+                                <b>ZKBioSecurity V5000 4.1.1 :</b> ZKBioSecurity is the
+                                ultimate “All-in-One” web-based security platform
+                                developed by ZKTeco. It contains multiple integrated
+                                modules: access control, time & attendance, consumption
+                                management, elevator control, visitor management,
+                                parking, guard patrol, entrance control, FaceKiosk, and
+                                intelligent facial recognition camera management,
+                                video(VMS), mask, and temperature detection module and
+                                other smart sub-systems. All of the sub-systems support
+                                both single-unit deployment and multi-module.
+                            </p>
+                        </div>
+                        <div class="footer">
+                            <div>
+                                <button>User Manual</button>
+                                <button>V5000 4.1.1</button>
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="logo">
+                        <img
+                                data-src="/img/software/zkbiosecurityV5000.png"
+                                class="lazyload"
+                                alt=""
+                            />
+                    </div>
+                </div>
+
+                <div class="containerStartWithImage">
+                    <div class="logo">
+                        <img
+                                data-src="/img/software/ZKBioHLMS.png"
+                                class="lazyload"
+                                alt=""
+                            />
+                    </div>
+                    <div class="content">
+                        <div class="header">
+                            <p>
+                            <h4> New Software Alert </h4>
+                            ZKBioHLMS is newly released for wireless smart hotels.
+                            </p>
+                        </div>
+                        <div class="wrapper">
+                            <p>
+                                <b>ZKBioHLMS :</b> ZKBioHLMS is an app-less wireless smart hotel platform developed by ZKTeco. 
+                                Without the need to download or register for an app, hotel guests could check-in and request 
+                                room services through an HTML5 user interface by clicking on a hyperlink provided in an 
+                                SMS or email automatically sent from ZKTeco’s cloud server before their stay. 
+                                ZKBioHLMS is considered a version of ZKBioSecurity. 
+                                It contains multiple integrated modules: hotel, access control, elevator control, personnel, 
+                                and other modules from ZKBioSecurity V6000, truly achieving smart deployment, smart operation, 
+                                and smart check-in.
+                            </p>
+                        </div>
+                        <div class="footer">
+                            <div>
+                                <button>User Manual</button>
+                                <button>V5000 4.1.1</button>
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+                <div class="container">
+                    <div class="content">
+                        <div class="header">
+                            <p>
+                            <h4> Update Alert </h4>
+                            Version 8.0.4 now available.
+                            </p>
+                        </div>
+                        <div class="wrapper">
+                            <p>
+                                <b>BioTime 8.0.4 :</b> a powerful web-based time and attendance management software 
+                                that provides a stable connection to ZKTeco’s standalone push communication devices by 
+                                Ethernet/Wi-Fi/GPRS/3G and working as a private cloud to offer employee self-service by 
+                                mobile application and web browser. BioTime 8.0 comes with an intuitive 
+                                user interface that is able to manage timetable, shift and schedule and can 
+                                easily generate attendance report.
+                            </p>
+                        </div>
+                        <div class="footer">
+                            <div>
+                                <button>User Manual</button>
+                                <!--button>App User Manual</button-->
+                                <button> Try Free</button>
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="logo">
+                        <img
+                                data-src="/img/software/biotime.png"
+                                class="lazyload"
+                                alt=""
+                            />
+                    </div>
+                </div>
+
+
+                <div class="containerStartWithImage">
+                    <div class="logo">
+                        <img
+                                data-src="/img/software/zkbiosecurityV320.png"
+                                class="lazyload"
+                                alt=""
+                            />
+                    </div>
+                    <div class="content">
+                        <div class="header">
+                            <p>
+                            <h4> New Software  </h4>
+                            ZKBioSecurity.
+                            </p>
+                        </div>
+                        <div class="wrapper">
+                            <p>
+                                <b>ZKBioSecurity 3.2.0.0 : </b> ZKBioSecurity is the ultimate “All-in-One” web-based security platform 
+                                developed by ZKTeco. It contains multiple integrated modules: access control, time & attendance, 
+                                consumption management, elevator control, visitor management, parking, guard patrol, entrance control, 
+                                FaceKiosk, and intelligent facial recognition camera management, video(VMS), mask, and temperature 
+                                detection module and other smart sub-systems. All of the sub-systems support both single-unit 
+                                deployment and multi-module.
+                            </p>
+                        </div>
+                        <div class="footer">
+                            <div>
+                                <button>User Manual</button>
+                                <button>Try Free</button>
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+                <div class="container">
+                    <div class="content">
+                        <div class="header">
+                            <p>
+                            <h4> Update Alert </h4>
+                                Time Net
+                            </p>
+                        </div>
+                        <div class="wrapper">
+                            <p>
+                                <b>Time.Net 3.3.1 : </b> is a time attendance management software. It integrates with Time Attendance 
+                                and Access Control systems. Frequently used functions such as attendance reports, device management, 
+                                and employee management can be managed directly on the home page which provides an excellent 
+                                user experience. Owing to the Pay code function, it can generate both time attendance records 
+                                and corresponding payroll in the software and easy to merge with the most ERP and Payroll 
+                                software, which can rapidly upgrade your working efficiency. The brand new flat GUI design 
+                                and humanized structure will make your daily management more pleasant and convenient.
+                            </p>
+                        </div>
+                        <div class="footer">
+                            <div>
+                                <button>User Manual</button>
+                                <button>Try Free</button>
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="logo">
+                        <img
+                                data-src="/img/software/zktimenet.png"
+                                class="lazyload"
+                                alt=""
+                            />
+                    </div>
+                </div>
+
+
+
+                <div class="containerStartWithImage">
+                    <div class="logo">
+                        <img
+                                data-src="/img/software/zkbiosecurityV320.png"
+                                class="lazyload"
+                                alt=""
+                            />
+                    </div>
+                    <div class="content">
+                        <div class="header">
+                            <p>
+                            <h4> New Software  </h4>
+                            ZKAccess.
+                            </p>
+                        </div>
+                        <div class="wrapper">
+                            <p>
+                                <b>ZKAccess 3.5 : </b>As a professional access control software, 
+                                ZKAccess3.5 is designed to manage all ZKTeco’s access control panels and standalone access 
+                                control terminals. The software can simultaneously manage access control and generate 
+                                attendance report. It’s a desktop software which is suitable for small and medium 
+                                businesses application. The brand new flat GUI design and humanized structure of 
+                                new ZKAccess3.5 will make your daily management more pleasant and convenient.
+                            </p>
+                        </div>
+                        <div class="footer">
+                            <div>
+                                <button>Download Free</button>
+                                <button>Public Key</button>
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BreadCrumb from '~/components/elements/BreadCrumb';
+
+export default {
+    head() {
+        return {
+            titleTemplate: 'Software',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Software'
+                }
+            ]
+        };
+    },
+    components: {
+        BreadCrumb
+    },
+    layout: 'layout-default-website',
+    data: () => {
+        return {
+            breadCrumb: [
+                {
+                    text: 'Home',
+                    url: '/'
+                },
+                {
+                    text: 'Software'
+                }
+            ]
+        };
+    },
+    async created() {
+        const payload = {};
+        const response = await this.$store.dispatch(
+            'website/getCaseStudies',
+            payload
+        );
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+$startWithContent : 3fr 1.3fr;
+$startWithImage : 1.3fr 3fr;
+
+@mixin switchContent($gridTemplateColumns){
+    display: grid;
+    grid-template-columns: $gridTemplateColumns;
+    grid-gap: 1.4em;
+    margin-top: 7em;
+    .content{
+        display: flex;
+        flex-direction: column;
+        .header{
+            background-color: #dff0d8;
+            padding-left: 1.3em;
+            border-left: .6em solid #cae6be;
+            margin-bottom: 1.3em;
+            h4{
+                color: #3c763d;
+            }
+        }
+        .wrapper{
+            p{
+                font-size: 1.1em
+            };
+        }
+        .footer{
+            display: flex;
+            //justify-content: space-around;
+            margin-top: 1em;
+            border-top: 1px solid #000;
+            div{
+                margin-top: 2em;
+                button{
+                    padding: .6em 2.4em;
+                    background-color: #7cbd27;
+                    font-size: 1.1em;
+                    font-weight:500;
+                    border-radius: 50px;
+                    color:#fff;
+                    margin-right: 3em;
+                }
+            }
+        }
+    }
+
+    .logo{
+        align-items: center;
+    }
+
+    
+    @media (max-width: 1200px){
+        // width: 900px;
+        background: green;
+        margin-right:auto;
+        margin-left: auto; 
+        .wrapper_001{
+            width: 900px;
+            margin-right:auto;
+            margin-left: auto;
+            
+            display: flex;
+            flex-direction:row;
+        }
+    }
+
+    @media (max-width: 900px) {
+        background: red;
+        // width: 800px;
+        // margin-right:auto;
+        // margin-left: auto; 
+    }
+
+    // @media (max-width: 900px){
+    //     width: 900px;
+    //     margin: 0 auto !important;
+    //     background: yellow;
+    //     .wrapper_001{
+    //         background: yellow;
+    //     }
+    // }
+
+    // @media (max-width: 600px) {@media (max-width: 480px) {
+    //         width: 100%;
+    //     }
+    
+}
+
+.ps-vendor-banner {
+    padding: 60px 0;
+    h2 {
+        color: black;
+        visibility: hidden;
+    }
+    // background: url('/img/bg/vendor.jpg');
+}
+.softwarebanner {
+    background: #000;
+    height: 30em;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+        width: 30em;
+        text-align: center;
+        font-size: 1.6em;
+        color: #fff;
+        letter-spacing: 1.5px;
+        
+
+        button {
+            outline: 1px solid #7cbd27;
+            font-size: 0.8em;
+            padding: 0.3em 1.3em;
+            border-radius: 40px;
+            margin-top: 1em;
+        }
+    }
+}
+
+.softwarecontainer {
+    
+    .wrapper_001{
+        width: 1200px;
+        margin-right:auto;
+        margin-left: auto;
+        
+        display: flex;
+        flex-direction:column;
+        .container{
+            @include switchContent($startWithContent)
+        }
+
+        .containerStartWithImage{
+            @include switchContent($startWithImage)
+        }
+    }
+}
+</style>
