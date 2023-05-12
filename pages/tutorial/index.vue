babe9005d86f9c22a38376cb3cb69f1060110145
Adesanwo Kazeem <adesanwo1@yahoo.com>
2023-05-12 16:44:10 +0100

    update on the componenet

--- pages/tutorial/index.vue
+++ pages/tutorial/index.vue
@@ -1,155 +1,38 @@
 <template lang="html">
     <div class="tutorial">
-        <section class="wrapper">
-            <section class="video-wrapper">
-                <section class="video">
-                    <header>
-                        <h3>{{ videoTitle }}</h3>
-                    </header>
-                    <div>
-                        <iframe
-                            width="100%"
-                            height="415"
-                            :src="videoSrc"
-                            :title="allVideos.length"
-                            frameborder="0"
-                            allowfullscreen
-                        ></iframe>
-                    </div>
-                </section>
-                <footer>
-                    <div>
-                        <div>Download</div>
-                    </div>
-                    <div>
-                        <div>Attitude Test</div>
-                    </div>
-                </footer>
-            </section>
-            <article class="menu-wrapper">
-                <header class="menu-header">
-                    <h3>Contents</h3>
-                </header>
-                <section class="menu-list">
-                    <div class="menu"  v-for="(video, i) in videoSubCategories">
-                        <header>
-                            <h4  @click="collapsible(i)">{{ video.name }}</h4>
-                        </header>
-
-                        <nav class="dropdown">
-                            <div
-                                :index="i"
-                                v-for="(allVideo, i) in video.tutorial_videos"
-                                @click="
-                                    changeVideoSrc(allVideo.url),
-                                        changeVideoTitle(allVideo.title)
-                                "
-                            >
-                                <span>{{ `${i + 1}. ${allVideo.title}` }}</span>
-                            </div>
-                        </nav>
-                    </div>
-                </section>
-            </article>
-        </section>
+        <Video
+            v-if="videoSubCategories"
+            :videos="videoSubCategories"
+            :loading="loading"
+        />
     </div>
 </template>
 
 <script>
-import { cloneWithoutLoc } from '@babel/types';
 import { mapState } from 'vuex';
+import Video from '~/components/partials/page/tutorial/Video';
 
 export default {
-    components: {},
+    components: { Video },
 
     transition() {
         return 'fadeIn';
     },
     layout: 'layout-default-website',
-    data() {
-        return {
-            videoSrc: null,
-            videoTitle: null,
-            musicIndex: 0,
-            isClicked: false,
-            allVideos: [
-                {
-                    name: 'How to Add admin to Device on ZKBioSecurity',
-                    src: 'https://www.youtube.com/embed/QGgz3QlaBjk',
-                    video: 'QGgz3QlaBjk',
-                    id: 'vid_1'
-                },
-                {
-                    name: 'How to Set Personnel Valid Times on ZKBioSecurity',
-                    src: 'https://www.youtube.com/embed/qIxde84wxj8',
-                    video: 'szH-m_W_8ss',
-                    id: 'vid_2'
-                },
-                {
-                    name: 'How to Setup Security Parameters on ZKBioSecurity',
-                    src: 'https://www.youtube.com/embed/vqLn-g7scXE',
-                    video: 'vqLn-g7scXE',
-                    id: 'vid_3'
-                },
-                {
-                    name: 'Computer Monitor Shape',
-                    src: 'https://www.youtube.com/embed/QGgz3QlaBjk',
-                    id: 'vid_4',
-                    video: 'QGgz3QlaBjk'
-                },
-                {
-                    name: 'Computer Monitor Shape',
-                    src: 'https://www.youtube.com/embed/szH-m_W_8ss',
-                    video: 'szH-m_W_8ss',
-                    id: 'vid_5'
-                },
-                {
-                    name: 'Computer Monitor Shape',
-                    src: 'https://www.youtube.com/embed/PSp60epZrvk',
-                    video: 'PSp60epZrvk',
-                    id: 'vid_6'
-                }
-            ]
-        };
-    },
 
     computed: {
         title() {
             return 'Product Videos';
         },
         ...mapState({
+            loading: state => state.website.loading,
             videoSubCategories: state => state.website.videoSubCategories
         })
     },
     async mounted() {
-        
-        await this.$store
-            .dispatch('website/getVideoSubCategoryBySlug', {
-                slug: 'security-solutions'
-            })
-            .then(data => {
-                this.videoSrc = this.videoSubCategories[0].tutorial_videos[
-                    this.musicIndex
-                ].url;
-                this.videoTitle = this.videoSubCategories[0].tutorial_videos[
-                    this.musicIndex
-                ].title;
-            });
-    },
-    methods: {
-        async changeVideoSrc(src) {
-            this.videoSrc = src;
-            //let allLiTags = event.target.tagName;
-        },
-
-        async changeVideoTitle(title) {
-            this.videoTitle = title;
-        },
-
-        collapsible(i) {
-            let menu = document.getElementsByClassName('menu');
-            menu[i+1].children[1].classList.toggle('active');
-        }
+        await this.$store.dispatch('website/getVideoSubCategoryBySlug', {
+            slug: 'security-solutions'
+        });
     }
 };
 </script>
@@ -220,63 +103,6 @@ export default {
                 h3 {
                     font-size: 2rem;
                     font-weight: normal;
-                }
-            }
-            .menu-list {
-                border-left: 0.05rem solid #dcd8d8;
-                border-right: 0.05rem solid #dcd8d8;
-                display: flex;
-                flex-direction: column;
-                max-height: 565px;
-                overflow: auto;
-
-                .menu {
-                    border-bottom: 1px solid #dcd8d8;
-                    cursor: pointer;
-
-                    header {
-                        display: flex;
-                        justify-content: space-between;
-                        justify-items: center;
-                        align-items: center;
-
-                        .mdi-chevron-right {
-                            display: flex;
-                            font-size: 2.5rem;
-                        }
-
-                        .rotate {
-                            transform: rotate(90deg);
-                        }
-                    }
-
-                    h4 {
-                        padding: 2rem 0 1rem 2rem;
-                        font-size: 1.4rem;
-                        font-weight: normal;
-                        font-weight: bold;
-                    }
-
-                    .dropdown {
-                        display: none;
-                        div {
-                            padding: 1.3rem 3rem;
-                            border-top: 1px solid #dcd8d8;
-                        }
-                        div:hover {
-                            background: #dcd8d8;
-                        }
-                    }
-
-                    .active {
-                        display: flex;
-                        flex-direction: column;
-                        visibility: visible;
-                    }
-                }
-
-                &::-webkit-scrollbar {
-                    width: 0px;
                 }
             }
         }
