19dc30a8308c0e35a22c87155f0d67369d4f9828
Adesanwo Kazeem <adesanwo1@yahoo.com>
2023-10-09 18:04:46 +0100

    worked on navigation bar

--- components/shared/menu/website/MenuSolutionCategories.vue
+++ components/shared/menu/website/MenuSolutionCategories.vue
@@ -53,6 +53,7 @@
                     <a href="https://ngteco.ng/">
                         Smart Home
                     </a>
+                    <i class="icon-chevron-right"></i>
                 </h4>
                 <ul class="mega-menu__list">
                     <li>
@@ -102,6 +103,7 @@ export default {
             color: #78bc27;
         }
     }
+
     .mega-menu {
         border: none;
         background: rgba(0, 0, 0, 0.7);
@@ -111,27 +113,64 @@ export default {
         box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
         padding: 15px 5px 10px;
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        row-gap: 51px;
+        grid-template-columns: 1fr;
+        row-gap: 12px;
 
         .mega-menu__column {
-            min-width: 250px;
-            padding: 0px 15px;
+            min-width: 200px;
+            padding: 0px 10px;
+            position: relative;
         }
 
         h4 {
+            display: flex;
+            justify-content: space-between;
+            position: relative;
+            justify-items: center;
+            align-items: center;
             a {
                 color: #78bc27;
                 font-weight: 600;
+                &:hover {
+                    color: white;
+                }
             }
+            i {
+                color: #78bc27;
+                font-size: 10px;
+                &:hover {
+                    color: white;
+                    font-weight: 600;
+                    cursor: pointer;
+                }
+            }
+        }
+
+        div {
+            ul {
+                min-width: 180px;
+                position: absolute;
+                top: 0;
+                left: 100%;
+                display: none;
+            }
+        }
+
+        div:hover > ul {
+            background: rgba(0, 0, 0, 0.7);
+            border-radius: 0 4px 4px 0;
+            display: flex;
+            flex-direction: column;
         }
 
         .mega-menu__list {
             li {
+                padding: 10px 15px;
                 a {
-                    color: white;
+                    color: #78bc27;
+                    display: block;
                     &:hover {
-                        color: #78bc27;
+                        color: white;
                     }
                 }
             }
